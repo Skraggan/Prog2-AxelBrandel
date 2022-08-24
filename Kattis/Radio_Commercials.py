5a60311666683619833d2ff1202b672b6084add4
AxelBrandel <84927377+Skraggan@users.noreply.github.com>
2022-08-24 10:06:13 +0200

    Kattis "Radio Commercials" done

--- Kattis/Radio_Commercials.py
+++ Kattis/Radio_Commercials.py
@@ -7,12 +7,14 @@ for i in range(0, len(eachComListens)):
     eachComListens[i] = int(eachComListens[i])
 
 currentBestProfit = 0
-
 for i in range(0, amountOfComs):
     currentProfit = 0
     for n in range(i, amountOfComs):
-        if eachComListens[n] > cost:
-            currentProfit += eachComListens[n] - cost
+        sequenceProfit = 0
+        for m in range(i, n):
+            sequenceProfit += eachComListens[m]-cost
+        if sequenceProfit > currentProfit:
+            currentProfit = sequenceProfit
     if currentProfit > currentBestProfit:
         currentBestProfit = currentProfit
 
