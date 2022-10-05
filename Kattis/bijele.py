cacb70956270319dd4de47c68569dfa2fbc5b402
AxelBrandel <84927377+Skraggan@users.noreply.github.com>
2022-10-05 10:02:38 +0200

    daöldawda

--- Kattis/bijele.py
+++ Kattis/bijele.py
@@ -1,8 +1,7 @@
-count = input().split(" ")
-whole = [1, 1, 2, 2, 2, 8]
+count = [int(i) for i in input().split(" ")]
 change = ""
 
 for i in range(6):
-    change += str((whole[i]-int(count[i]))) + " "
+    change += str(([1, 1, 2, 2, 2, 8][i]-count[i])) + " "
 
 print(change)
