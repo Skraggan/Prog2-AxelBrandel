2be832dbbda2e6f825f5dee07d4f55609d883abf
AxelBrandel <84927377+Skraggan@users.noreply.github.com>
2022-12-05 15:20:55 +0100

    minesweeper update

--- tkinter/minesweeper/minesweepa.py
+++ tkinter/minesweeper/minesweepa.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import math
-from PIL import Image
+from PIL import Image, ImageTk
+
 
 window = tk.Tk()
 window.title("MINESWEEPA")
@@ -10,22 +11,40 @@ width, height = int(1920/2), int(1080/2)
 frame = tk.Frame(window)
 frame.pack()
 
-n_rows = 10
-n_cols = 10
+n_rows = 5
+n_cols = 5
 
-button_width = width/n_cols
-button_height = height/n_rows
+button_size = int((height-24)/n_rows)
 
 image = Image.open(r"tkinter/minesweeper/tile_plain.gif")
-print(image.size)
-# im = tk.PhotoImage(file="tkinter/minesweeper/tile_plain.gif")
+image = image.resize((button_size,button_size), 2)
+im = ImageTk.PhotoImage(image)
 
+text = tk.Label(frame, text="Yuuuuh", font=("Times new roman", 18))
+text.grid(column=0, columnspan=n_cols, row=0)
+
+tiles = dict()
 count = 1
 for r in range(n_rows):
     for c in range(n_cols):
-        button = tk.Button(frame, image=image)
-        # button.place(x=c*button_width, y=r*button_height, width=button_width, height=button_height)
-        button.grid(column=c, row=r, )
+        if c == 0:
+            tiles[r] = {}
+        
+        tile = {
+            "isMine": False,
+            "cords": {
+                "c": c,
+                "r": r
+            },
+            "button": tk.Button(frame, image=im),
+            "mines": 0,
+            "id": count
+        }
+        tile["button"].grid(column=c, row=r+1)
+        tiles[r][c] = tile
+
         count += 1
+
+print(tiles[0][0])
 
 frame.mainloop()
