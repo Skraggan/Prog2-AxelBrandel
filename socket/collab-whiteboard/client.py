b11fdc9bd174105f2ca14f8e891ba564510a3a38
AxelBrandel <84927377+Skraggan@users.noreply.github.com>
2023-02-06 15:28:28 +0100

    raah

--- socket/collab-whiteboard/client.py
+++ socket/collab-whiteboard/client.py
@@ -10,19 +10,28 @@ root.title("Collaborative Whiteboard")
 canvas = tk.Canvas(root, width=900, height=500, cursor="hand2")
 canvas.place(x=115, y=35)
 
+draw_size = 5
 current_x = 0
 current_y = 0
 
 def locate_xy(draw):
     global current_x, current_y
     current_x, current_y = draw.x, draw.y
+    point_size = abs(draw_size-3)
+    circle = canvas.create_oval(current_x-point_size, current_y-point_size, current_x+point_size, current_y+point_size, fill="black")
 
 def draw_line(draw):
     global current_x, current_y
-    line = canvas.create_line(current_x, current_y, draw.x, draw.y, width=2, fill="black")
+    line = canvas.create_line(current_x, current_y, draw.x, draw.y, width=draw_size, fill="black", capstyle=tk.ROUND, smooth=True)
     current_x, current_y = draw.x, draw.y
 
 canvas.bind("<Button-1>", locate_xy)
 canvas.bind("<B1-Motion>", draw_line)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
 
 root.mainloop()
--- socket/collab-whiteboard/server.py
+++ socket/collab-whiteboard/server.py
@@ -1,2 +1,17 @@
 import socket
 import threading
+
+ip, port = socket.gethostname(), 1234
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((ip, port))
+s.listen(5)
+print(f"Server is up and running on {ip}:{port}")
+
+sockets_list = [s]
+clients = {}
+
+while True:
+    conn, addr = s.accept()
+    print(f"Connection from {addr} had been established!")
+    conn.send(bytes("You have been connected!", "utf-8"))
